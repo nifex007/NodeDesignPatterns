a5d0133f0552fdb759641f02c1945238dd6469b7
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-07-10 23:17:25 +0100

    first commit (starting.. decoratorators)

---
+++ decorators/InventoryItem.js
@@ -0,0 +1,15 @@
+/**
+ * Pattern Name: Decorator
+ * The Problem: 
+ * The Solution:
+ * Consequence: 
+ * 
+ * 
+ * Decorators are design patterns that allows us to dynamically attach additonal properties, methods to existing objects
+ * Decorators take in a base item/class and extends the functionality
+ * 
+ * 
+ * Intent: Attach additional responsiblities to an object dynamically. Decorators provide a flexible alternative to subclassing
+ * for extending functionality
+ * 
+ */ 
---
+++ decorators/index.js
@@ -0,0 +1,15 @@
+const Shopper = require('./Shopper');
+const InventoryItem = require('/InventoryItem');
+
+const jon = new Shopper('Jon', 100000);
+
+const gShock = new InventoryItem("G-Shock Watch", 9999.99)
+const necklace = new InventoryItem("Necklace", 999.99)
+
+
+jon.purchase(necklace);
+jon.purchase(gShock);
+
+
+jon.printStatus();
+
