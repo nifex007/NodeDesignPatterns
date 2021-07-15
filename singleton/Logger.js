c2978dd43d51266a6a35046476f7e108a8af9e9d
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-07-15 23:28:24 +0100

    Moved singleton comments to index from Logger

--- singleton/Logger.js
+++ singleton/Logger.js
@@ -1,10 +1,3 @@
-/**
- * Singleton 
- * Creates only one instance of a class no matter how many times it is the class is being
- * instantiated.
- * 
- **/
-
 class Logger {
     constructor() {
         this.logs= [];
---
+++ singleton/index.js
@@ -0,0 +1,6 @@
+/**
+ * Singleton 
+ * Creates only one instance of a class no matter how many times it is the class is being
+ * instantiated.
+ * 
+ **/
