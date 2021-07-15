2bb343f239d89e418058a5b47053238a7c38a774
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-07-15 23:51:19 +0100

    index (invoking the classes)

--- singleton/index.js
+++ singleton/index.js
@@ -4,3 +4,32 @@
  * instantiated.
  * 
  **/
+
+const Logger = require('./Logger');
+const Shopper = require('./Shopper');
+const Store = require('./Store');
+
+
+const logger = new Logger();
+
+logger.log('starts app..');
+
+const jon = new Shopper('Jon', '5000');
+const ski_shop = new Store(`Steep & Deep Supplies`, [
+    {
+        item: 'Boots',
+        qty: 1,
+        price: 200
+    },
+    {
+        item: 'Shirt',
+        qty: 1,
+        price: 100
+    }
+])
+
+
+logger.log('finshing config ...');
+
+console.log(`${logger.count} logs total`);
+logger.logs.map(log => console.log(log.message));
