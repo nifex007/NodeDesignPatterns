0c0ed860d10a211fdb828a04323f0fc94e6fcbc8
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-07-17 23:19:25 +0100

    typo

--- singleton/Shopper.js
+++ singleton/Shopper.js
@@ -5,7 +5,7 @@ class Shopper {
     constructor(name, money =0) {
         this.name = name;
         this.money = money;
-        logger.log(`New Shopper: ${name} has ${money} in their accout.`);
+        logger.log(`New Shopper: ${name} has ${money} in their account.`);
     }
 }
 
